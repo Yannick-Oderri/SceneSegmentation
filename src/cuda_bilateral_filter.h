274b13bd3619629d28ccca0d213cd030636faa67
Yannick Roberts <contact@yannickrobets.com>
2019-12-30 23:49:08 -0500

    Fixed Sobel Pipeline Rendering
    Signed-off-by: Yannick Roberts <contact@yannickrobets.com>

---
+++ src/cuda_bilateral_filter.h
@@ -0,0 +1,14 @@
+//
+// Created by ynki9 on 12/30/19.
+//
+
+#ifndef PROJECT_EDGE_CUDA_BILATERAL_FILTER_H
+#define PROJECT_EDGE_CUDA_BILATERAL_FILTER_H
+
+
+class cuda_bilateral_filter {
+
+};
+
+
+#endif //PROJECT_EDGE_CUDA_BILATERAL_FILTER_H
