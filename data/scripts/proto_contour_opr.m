78c0868928054e0dba6e991b802e4cde602db072
Yannick Roberts <contact@yannickrobets.com>
2020-08-05 19:31:43 -0400

    working contours

--- data/scripts/proto_contour_opr.m
+++ data/scripts/proto_contour_opr.m
@@ -3,52 +3,62 @@
 % tx_angle = imread('/home/ynki9/Dev/ucf_research/project_edge/cmake-build-debug/test/results/testx.png');
 depth = imread('/home/ynki9/Dev/ucf_research/project_edge/data/images/depth/test65.png');
 
+%% Load yml
+width = 640;
+height = 576;
+data = fopen('spurs.yml');
+f = textscan(data, '%s', 'Delimiter', ',');
+v = str2double(f{:});
+gv = reshape(v, width, height).';
+spurs= gv;
 
 %% Load yml Color Image
 data = fopen('normals.yml');
 f = textscan(data, '%s', 'Delimiter', ',');
 v = str2double(f{:});
-gv = reshape(v, 4, 640, 480);
+gv = reshape(v, 4, width, height);
 gv = gv(1:3, :, :);
 normals = permute(gv, [3, 2, 1]);
 
 %% Load yml Image
-% data = fopen('hangle.yml');
-% f = textscan(data, '%s', 'Delimiter', ',');
-% v = str2double(f{:});
-% gv = reshape(v, 640, 480).';
-% % 
-% hangle= gv;
+data = fopen('hangle.yml');
+f = textscan(data, '%s', 'Delimiter', ',');
+v = str2double(f{:});
+gv = reshape(v, width, height).';
+% 
+hangle= gv;
 
 data = fopen('lrdir.yml');
 f = textscan(data, '%s', 'Delimiter', ',');
 v = str2double(f{:});
-gv = reshape(v, 640, 480).';
+gv = reshape(v, width, height).';
 lrdir= gv;
 
 
 %% Generate Gradient Map
+width = 640;
+height = 576
 
-% data = fopen('ygrad.yml');
-% f = textscan(data, '%s', 'Delimiter', ',');
-% v = str2double(f{:});
-% gv = reshape(v, 640, 480).';
-% ygrad = gv;
-% 
-% 
-% data = fopen('xgrad.yml');
-% f = textscan(data, '%s', 'Delimiter', ',');
-% v = str2double(f{:});
-% gv = reshape(v, 640, 480).';
-% xgrad = gv;
+data = fopen('ygrad.yml');
+f = textscan(data, '%s', 'Delimiter', ',');
+v = str2double(f{:});
+gv = reshape(v, width, height).';
+ygrad = gv;
+bwmorph
 
-txgrad = reshape(xgrad, [1 640*480]);
-tygrad = reshape(ygrad, [1 640*480]);
-uy = repelem(1:480, 640);
-tty =reshape(uy, [640 480]);
-uy = reshape(tty.', [1 640*480]);
-ux = repelem(1:640, 480);
-ttx =reshape(ux, [480 640]);
-ux = reshape(ttx, [1 640*480]);
+data = fopen('xgrad.yml');
+f = textscan(data, '%s', 'Delimiter', ',');
+v = str2double(f{:});
+gv = reshape(v, width, height).';
+xgrad = gv;
+
+txgrad = reshape(xgrad, [1 width*height]);
+tygrad = reshape(ygrad, [1 width*height]);
+uy = repelem(1:height, width);
+tty =reshape(uy, [width height]);
+uy = reshape(tty.', [1 width*height]);
+ux = repelem(1:width, height);
+ttx =reshape(ux, [height width]);
+ux = reshape(ttx, [1 width*height]);
 figure;
 q = quiver(ux, uy*-1, txgrad, tygrad);
